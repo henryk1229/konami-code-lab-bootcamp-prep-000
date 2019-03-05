34e121a7eecb9ccefd8279fb262db930f1e2b762
Henry Koehler <henryk1229@gmail.com>
2019-03-05 22:44:15 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -12,12 +12,12 @@ const codes = [
 ];
 
 function init() {
-  let idx = 0;
+  let index = 0;
 
   document.body.addEventListener("keydown", (e) => {
     const key = e.key;
     
-    idx = (codes[idx] === key) ? ++idx : 0;
+    indexdx = (codes[i] === key) ? ++idx : 0;
     
     if (idx === codes.length) {
       window.alert("Hurray!");
